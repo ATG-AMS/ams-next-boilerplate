3dabca14f66440675fd286a14783814203a6779a
hoon-ch <hun9211@gmail.com>
2024-03-07 00:47:55 +0000

    🩺 Update button and select styles

--- src/components/atoms/Select.tsx
+++ src/components/atoms/Select.tsx
@@ -26,7 +26,7 @@ const SelectTrigger = React.forwardRef<
   >
     {children}
     <SelectPrimitive.Icon asChild>
-      <RxCaretSort className="h-4 w-4 opacity-50" />
+      <RxCaretSort className="size-4 opacity-50" />
     </SelectPrimitive.Icon>
   </SelectPrimitive.Trigger>
 ));
@@ -86,9 +86,9 @@ const SelectItem = React.forwardRef<
     )}
     {...props}
   >
-    <span className="absolute right-2 flex h-3.5 w-3.5 items-center justify-center">
+    <span className="absolute right-2 flex size-3.5 items-center justify-center">
       <SelectPrimitive.ItemIndicator>
-        <RxCheck className="h-4 w-4" />
+        <RxCheck className="size-4" />
       </SelectPrimitive.ItemIndicator>
     </span>
     <SelectPrimitive.ItemText>{children}</SelectPrimitive.ItemText>
